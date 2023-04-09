f7db6402d9342680c543088970601dc0b4edcef9
ZephirSD <siegfrieddallery@gmail.com>
2023-04-09 10:42:30 +0400

    Création des Routes avec la base `menu_header`

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import  './style/style.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageModel from './pages/PageModel';
+import { menu } from './components/base/menu_header';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          {
+            menu.map((mn, index)=> (
+              <Route key={index} path={mn.permalien} element={<PageModel/>} />
+            ))
+          }
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
