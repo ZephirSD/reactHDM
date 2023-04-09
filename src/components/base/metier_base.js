3d0f6b22b83581dc1f7fef3a73e07c3a3b762b9e
ZephirSD <siegfrieddallery@gmail.com>
2023-04-09 10:44:58 +0400

    Création de la base `metiers` pour les Cartes dans la page d'Accueil.

---
+++ src/components/base/metier_base.js
@@ -0,0 +1,6 @@
+export const metiers = [
+    { titre: "Audit", para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, exercitationem quos alias molestiae, cum quae sint commodi sunt consequuntur, iste odit illo facere iusto rerum tenetur animi veritatis? Perspiciatis, labore! Aliquid debitis non autem eius?"},
+    { titre: "Expertise comptable", para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, exercitationem quos alias molestiae, cum quae sint commodi sunt consequuntur, iste odit illo facere iusto rerum tenetur animi veritatis? Perspiciatis, labore! Aliquid debitis non autem eius?"},
+    { titre: "Gestionnaire de paie, accompagnement RH", para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, exercitationem quos alias molestiae, cum quae sint commodi sunt consequuntur, iste odit illo facere iusto rerum tenetur animi veritatis? Perspiciatis, labore! Aliquid debitis non autem eius?" },
+    { titre: "Conseil en organisation", para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, exercitationem quos alias molestiae, cum quae sint commodi sunt consequuntur, iste odit illo facere iusto rerum tenetur animi veritatis? Perspiciatis, labore! Aliquid debitis non autem eius?" }
+];
