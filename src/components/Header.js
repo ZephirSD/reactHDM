6bbd88094639c5410e104096c121e7513a56b33c
ZephirSD <siegfrieddallery@gmail.com>
2023-04-09 10:44:19 +0400

    Création du composant Header avec l'animation du bounce lors du changement de page

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { menu } from './base/menu_header'
+import { motion } from "framer-motion";
+import { useState } from 'react';
+
+function Header() {
+    const [switchPath, setSwitchPath] = useState("");
+    return (
+        <>
+        <header>
+            <div></div>
+            <nav>
+                <ul>
+                    {
+                        menu.map((mn,index) => (
+                            <li key={index} onClick={() => setSwitchPath(mn.permalien.replace("/", ""))}>
+                                <Link to={mn.permalien} style={switchPath === mn.permalien.replace("/", "") ? {color: "white",textDecoration: "none"} : {color: "var(--greyText)",textDecoration: "none"}}>{mn.titre}</Link>
+                                { 
+                                    mn.permalien.replace("/", "") === switchPath ? 
+                                    (
+                                        <motion.div className="case_toggle" layoutId="case_toggle"></motion.div>
+                                    ) :
+                                    (
+                                        <></>
+                                    )
+                                }
+                            </li>
+                        ))
+                    }
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header
