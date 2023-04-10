301a43ea46de08111d99f1099f3aaafa83b7ccc9
ZephirSD <siegfrieddallery@gmail.com>
2023-04-10 19:41:40 +0400

    Ajout du composant Associes dans le menu

--- src/components/base/menu_header.js
+++ src/components/base/menu_header.js
@@ -1,9 +1,10 @@
 import Accueil from "../../pages/Accueil";
+import Associes from "../../pages/Associes";
 import Metiers from "../../pages/Metiers";
 
 export const menu = [
     { titre: "Accueil", permalien: "/", components: <Accueil/> },
     { titre: "Nos Métiers", permalien: "/nos-metiers", components: <Metiers/>},
-    { titre: "Nos Associés", permalien: "/nos-associes", components: <Accueil/> },
+    { titre: "Nos Associés", permalien: "/nos-associes", components: <Associes/> },
     { titre: "Contact", permalien: "/contact", components: <Accueil/> }
 ];
