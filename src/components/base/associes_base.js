6801f3904e9dd2fb33930c950470273bde0b490f
ZephirSD <siegfrieddallery@gmail.com>
2023-04-10 19:42:13 +0400

    Création de la  base `associes_base` pour les cartes Associes

---
+++ src/components/base/associes_base.js
@@ -0,0 +1,77 @@
+import man from '../../assets/imageAsso/man.jpg';
+import woman from '../../assets/imageAsso/woman.jpg';
+
+export const associes = [
+    {
+        id:1,
+        nomPrenom: "Test test",
+        metier: "Lorem ipsum dolor sit amet.",
+        secteur: "Lorem, ipsum dolor.",
+        telephone: "0692 00 00 00",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: man
+    },
+    {
+        id:2,
+        nomPrenom: "Test test 2",
+        metier: "Lorem ipsum dolor sit amet.", 
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: woman
+    },
+    {
+        id:3, 
+        nomPrenom: "Test test 3", 
+        metier: "Lorem ipsum dolor sit amet.",
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: man
+    },
+    {
+        id:4, 
+        nomPrenom: "Test test 4", 
+        metier: "Lorem ipsum dolor sit amet.", 
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: man
+    },
+    {
+        id:5, 
+        nomPrenom: "Test test 5", 
+        metier: "Lorem ipsum dolor sit amet.", 
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: man
+    },
+    {
+        id:6, 
+        nomPrenom: "Test test 6", 
+        metier: "Lorem ipsum dolor sit amet.", 
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: man
+    },
+    {
+        id:7, 
+        nomPrenom: "Test test 7", 
+        metier: "Lorem ipsum dolor sit amet.", 
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: woman
+    },
+    {
+        id:8, 
+        nomPrenom: "Test test 8", 
+        metier: "Lorem ipsum dolor sit amet.", 
+        secteur: "Lorem, ipsum dolor.", 
+        telephone: "0692 00 00 00", 
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem incidunt sequi maiores, enim quam!",
+        image: woman
+    }
+];
