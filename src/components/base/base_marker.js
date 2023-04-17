23e2e20173df01d3e4986b38e96f6e5623053219
ZephirSD <siegfrieddallery@gmail.com>
2023-04-17 20:07:01 +0400

    Création de la base markerRun pour les markers de Carte

---
+++ src/components/base/base_marker.js
@@ -0,0 +1,30 @@
+export const markerRun = [
+    {
+        lieu: 'Lorem Saint Denis',
+        addresse: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        num_tel: '0262 00 00 00',
+        latitude: -20.8789,
+        longitude: 55.4481
+    },
+    {
+        lieu: 'Lorem Saint Benoit',
+        addresse: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        num_tel: '0262 00 00 00',
+        latitude: -21.0339,
+        longitude: 55.7128
+    },
+    {
+        lieu: 'Lorem Saint Pierre',
+        addresse: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        num_tel: '0262 00 00 00',
+        latitude: -21.3334,
+        longitude: 55.4737
+    },
+    {
+        lieu: 'Lorem Le Port',
+        addresse: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        num_tel: '0262 00 00 00',
+        latitude: -20.9394,
+        longitude: 55.2872
+    }
+]
