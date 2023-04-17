fbdd9d985b4c95fb971915dd5ed7715c274133a4
ZephirSD <siegfrieddallery@gmail.com>
2023-04-17 20:12:56 +0400

    Insertion du composant Contact dans le menu

--- src/components/base/menu_header.js
+++ src/components/base/menu_header.js
@@ -1,10 +1,11 @@
 import Accueil from "../../pages/Accueil";
 import Associes from "../../pages/Associes";
+import Contact from "../../pages/Contact";
 import Metiers from "../../pages/Metiers";
 
 export const menu = [
     { titre: "Accueil", permalien: "/", components: <Accueil/> },
     { titre: "Nos Métiers", permalien: "/nos-metiers", components: <Metiers/>},
     { titre: "Nos Associés", permalien: "/nos-associes", components: <Associes/> },
-    { titre: "Contact", permalien: "/contact", components: <Accueil/> }
+    { titre: "Contact", permalien: "/contact", components: <Contact/> }
 ];
