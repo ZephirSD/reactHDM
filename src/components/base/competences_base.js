65af5cf3aa627179388d28bb60fa82f71e763708
ZephirSD <siegfrieddallery@gmail.com>
2023-04-22 18:51:25 +0400

    Création de la base pour les différentes cases de la page Metiers

---
+++ src/components/base/competences_base.js
@@ -0,0 +1,110 @@
+import image_metier1 from '../../assets/image_metier1.jpg';
+import image_metier2 from '../../assets/image_metier2.jpg';
+import image_metier3 from '../../assets/image_metier3.jpg';
+import image_metier4 from '../../assets/image_metier4.jpg';
+
+export const comptencesBase = [
+    {
+        titre_case: 'Audit',
+        direction_titre_case: 'flex-start',
+        intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque est error iure exercitationem similique in commodi reiciendis alias! Exercitationem quasi adipisci rerum sed similique saepe!',
+        titre: 'Lorem ipsum dolor sit amet.',
+        descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt voluptatum delectus velit non id enim blanditiis nobis fuga eos magnam eum molestias quam corrupti facere dignissimos nulla, vero at impedit ducimus. Sint facilis ducimus quaerat, in recusandae deserunt ipsum ut, vitae alias laboriosam veniam quisquam.',
+        titre_point: 'Lorem ipsum dolor sit amet consectetur.',
+        points: [
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+        ],
+        image: image_metier1,
+        listes_competences : [
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+        ],
+        order_texte: 0,
+        order_image: 1
+    },
+    {
+        titre_case: 'Expertise comptable',
+        direction_titre_case: 'flex-end',
+        intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque est error iure exercitationem similique in commodi reiciendis alias! Exercitationem quasi adipisci rerum sed similique saepe!',
+        titre: 'Lorem ipsum dolor sit amet.',
+        descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt voluptatum delectus velit non id enim blanditiis nobis fuga eos magnam eum molestias quam corrupti facere dignissimos nulla, vero at impedit ducimus. Sint facilis ducimus quaerat, in recusandae deserunt ipsum ut, vitae alias laboriosam veniam quisquam.',
+        titre_point: 'Lorem ipsum dolor sit amet consectetur.',
+        points: [
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+        ],
+        image: image_metier2,
+        listes_competences : [
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+        ],
+        order_texte: 1,
+        order_image: 0
+    },
+    {
+        titre_case: 'Gestionnaire de paie, accompagnement RH',
+        direction_titre_case: 'flex-start',
+        intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque est error iure exercitationem similique in commodi reiciendis alias! Exercitationem quasi adipisci rerum sed similique saepe!',
+        titre: 'Lorem ipsum dolor sit amet.',
+        descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt voluptatum delectus velit non id enim blanditiis nobis fuga eos magnam eum molestias quam corrupti facere dignissimos nulla, vero at impedit ducimus. Sint facilis ducimus quaerat, in recusandae deserunt ipsum ut, vitae alias laboriosam veniam quisquam.',
+        titre_point: 'Lorem ipsum dolor sit amet consectetur.',
+        points: [
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+        ],
+        image: image_metier3,
+        listes_competences : [
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+        ],
+        order_texte: 0,
+        order_image: 1
+    },
+    {
+        titre_case: 'Conseil en organisation',
+        direction_titre_case: 'flex-end',
+        intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque est error iure exercitationem similique in commodi reiciendis alias! Exercitationem quasi adipisci rerum sed similique saepe!',
+        titre: 'Lorem ipsum dolor sit amet.',
+        descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt voluptatum delectus velit non id enim blanditiis nobis fuga eos magnam eum molestias quam corrupti facere dignissimos nulla, vero at impedit ducimus. Sint facilis ducimus quaerat, in recusandae deserunt ipsum ut, vitae alias laboriosam veniam quisquam.',
+        titre_point: 'Lorem ipsum dolor sit amet consectetur.',
+        points: [
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+            'Lorem ipsum dolor sit amet consectetur.',
+        ],
+        image: image_metier4,
+        listes_competences : [
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+            'Lorem ipsum dolor.',
+        ],
+        order_texte: 1,
+        order_image: 0
+    }
+]
